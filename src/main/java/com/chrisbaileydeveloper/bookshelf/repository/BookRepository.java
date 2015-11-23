2d3d32d3dbf80c6aedb662db9e902493bee05d08
chris-bailey <chris@chrisbaileydeveloper.com>
2015-11-23 16:35:21 -0500

    Polish

--- src/main/java/com/chrisbaileydeveloper/bookshelf/repository/BookRepository.java
+++ src/main/java/com/chrisbaileydeveloper/bookshelf/repository/BookRepository.java
@@ -1,7 +1,5 @@
 package com.chrisbaileydeveloper.bookshelf.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.chrisbaileydeveloper.bookshelf.domain.Book;
